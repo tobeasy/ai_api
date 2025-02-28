8ad2e7c761456dec02dee08b3ea3c0b04258b30a
Tobias <tobias.schmelzle@gmx.de>
2025-03-01 00:20:27 +0100

    display api_keys in input field and rearange buttons

--- Streamlit_ai.py
+++ Streamlit_ai.py
@@ -2,6 +2,8 @@ import streamlit as st
 import request_distributor
 import database
 
+
+
 # Initialize Request_distributor and Database instances in session state if not already done
 if 'request_distributor' not in st.session_state:
     st.session_state.request_distributor = request_distributor.Request_distributor()
@@ -17,12 +19,18 @@ st.title('Universal AI Client')
 options = ['Gemini 2.0 Flash', 'Gemini-2.0-flash-lite Preview', 'Gemini 1.5 Flash', 'Deepseek']
 selected_option = st.selectbox('Select an AI model:', options)
 
-api_key = st.text_input('Api_key:')
-if st.button('Save api-key'):
-    database.add_entry(selected_option, api_key)
+api_key = st.text_input('Api_key:', value=database.get_api_key(selected_option))
+col1, col2 = st.columns(2)
+with col1:
+    if st.button('Save api-key'):
+        database.add_entry(selected_option, api_key)
 
-if st.button('Delete api-key'):
-    database.remove_entry(selected_option)
+with col2:
+    if st.button('Delete api-key'):
+        database.remove_entry(selected_option)
+
+if st.button('Show all api-keys'):
+    st.write(database.get_all_entries())
 
 user_input = st.text_input('Enter a request:')
 
@@ -31,5 +39,3 @@ if st.button('Senden'):
     response = request_distributor.send_request(selected_option, user_input, api_key)
     st.write(response)
 
-if st.button('Show all api-keys'):
-    st.write(database.get_all_entries())
