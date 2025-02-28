19898473d2f5e569d6e14ca040776b09c572c29c
tobias <tobias.schmelzle@gmx.de>
2025-02-28 19:56:10 +0100

    finished deepseek api

--- apis/deepseek_api.py
+++ apis/deepseek_api.py
@@ -6,20 +6,15 @@ class deepseek_api:
 
     def __init__(self, api_key):
         self.client = OpenAI(api_key=api_key, base_url="https://api.deepseek.com")
-        self.messages=[
-                {"role": "system", "content": "You are a helpful assistant"},
-                {"role": "user", "content": ""},
-            ]
+        self.messages = []
 
     def send_request(self, text):
-        self.messages[-1]["content"] = text
+        self.messages.append({"role": "system", "content": "You are a helpful assistant"})
+        self.messages.append({"role": "user", "content": text})
         response = self.client.chat.completions.create(
             model="deepseek-chat",
             messages=self.messages,
             stream=False
         )
         self.messages.append(response.choices[0].message)
-        if response.status_code != 200:
-            return "Error contacting Deepseek. Status code: " + str(response.status_code)
-        else:
-            return response.choices[0].message.content
+        return response.choices[0].message.content
